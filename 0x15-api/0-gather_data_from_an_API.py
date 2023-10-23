787dcba9de3dcacd5a907b7d208b576dd939ee07
Onsomujosh <Joshuaonsomu@gmail.com>
2023-10-23 15:25:36 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,35 @@ from requests import get
 from sys import argv
 
 if __name__ == '__main__':
-    main_url = 'https://jsonplaceholder.typicode.com'
+    if len(argv) != 2:
+        print("Usage: python script.py <employee_id>")
+    else:
+        employee_id = argv[1]
+        main_url = 'https://jsonplaceholder.typicode.com'
+        todo_url = main_url + "/todos?userId={}".format(employee_id)
+        user_url = main_url + "/users/{}".format(employee_id)
+
+        try:
+            todo_response = get(todo_url)
+            user_response = get(user_url)
+
+            if todo_response.status_code == 200 and user_response.status_code == 200:
+                todo_data = todo_response.json()
+                user_data = user_response.json()
+
+                employee_name = user_data.get("name")
+                total_tasks = len(todo_data)
+                completed_tasks = sum(1 for task in todo_data if task["completed"])
+
+                print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
+                for task in todo_data:
+                    if task["completed"]:
+                        print("\t{}".format(task["title"]))
+            else:
+                print("Error: Unable to fetch data. Please check the employee ID.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+    """main_url = 'https://jsonplaceholder.typicode.com'
     todo_url = main_url + "/user/{}/todos".format(argv[1])
     name_url = main_url + "/users/{}".format(argv[1])
     todo_result = get(todo_url).json()
@@ -21,4 +49,4 @@ if __name__ == '__main__':
           .format(name, todo_complete, todo_num))
     for todo in todo_result:
         if (todo.get("completed")):
-            print("\t {}".format(todo.get("title")))
+            print("\t {}".format(todo.get("title")))"""
