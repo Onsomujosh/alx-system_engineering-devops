dfd38692a3615e659f7db275cef535856e369f21
Onsomujosh <joshuaonsomu@gmail.com>
2023-10-25 14:06:08 -0700

    task 0 fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-'''
-This script returns information about an employee's TODO list progress using a REST API.
-Usage: python script_name.py employee_id
-'''
+'''This script  returns information about his/her
+TODO list progress using RestApi'''
 import requests
 import sys
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py employee_id")
-        sys.exit(1)
 
+if __name__ == '__main__':
     try:
         employee_id = int(sys.argv[1])
     except ValueError:
-        print("Invalid employee ID. Please provide a valid integer ID.")
-        sys.exit(1)
+        exit()
 
     base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todo_url = f'{user_url}/todos'
+    user_url = '{api}/users/{id}'.format(api=base_url, id=employee_id)
+    todo_url = '{user_url}/todos'.format(user_url=user_url)
 
-    user_response = requests.get(user_url).json()
-    employee_name = user_response.get('name')
-
-    todo_response = requests.get(todo_url).json()
-    total_tasks = len(todo_response)
-    non_completed = sum(1 for task in todo_response if not task['completed'])
+    '''get the user's response'''
+    res = requests.get(user_url).json()
+    '''get the employee name'''
+    name = res.get('name')
+    '''Get the todo response'''
+    res = requests.get(todo_url).json()
+    '''get total number of tasks'''
+    total_tasks = len(res)
+    '''get the incompleted tasks'''
+    non_completed = sum([elem['completed'] is False for elem in res])
+    '''get completed tasks'''
     completed_tasks = total_tasks - non_completed
-
-    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
-    for task in todo_response:
-        if task['completed']:
-            print(f"\t{task['title']}")
+    str = ("Employee {emp_name} is done with tasks" +
+           "({completed_tasks}/{total_tasks}): ")
+    print(str.format(emp_name=name, completed_tasks=completed_tasks,
+                     total_tasks=total_tasks))
+    '''print the the complete tasks'''
+    for elem in res:
+        if elem.get('completed') is True:
+            print('\t', elem.get('title'))
