6e443f4d64529807c09be35edf1f1654cd394abc
Onsomujosh <joshuaonsomu@gmail.com>
2023-10-25 14:04:43 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-"""
-Uses the JSON placeholder api to query data about an employee
-"""
-
-from requests import get
-from sys import argv
+'''
+This script returns information about an employee's TODO list progress using a REST API.
+Usage: python script_name.py employee_id
+'''
+import requests
+import sys
 
 if __name__ == '__main__':
-    main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + "/user/{}/todos".format(argv[1])
-    name_url = main_url + "/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py employee_id")
+        sys.exit(1)
 
-    todo_num = len(todo_result)
-    todo_complete = len([todo for todo in todo_result
-                         if todo.get("completed")])
-    name = name_result.get("name")
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, todo_complete, todo_num))
-    for todo in todo_result:
-        if (todo.get("completed")):
-            print("\t {}".format(todo.get("title")))
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Invalid employee ID. Please provide a valid integer ID.")
+        sys.exit(1)
+
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{employee_id}'
+    todo_url = f'{user_url}/todos'
+
+    user_response = requests.get(user_url).json()
+    employee_name = user_response.get('name')
+
+    todo_response = requests.get(todo_url).json()
+    total_tasks = len(todo_response)
+    non_completed = sum(1 for task in todo_response if not task['completed'])
+    completed_tasks = total_tasks - non_completed
+
+    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+    for task in todo_response:
+        if task['completed']:
+            print(f"\t{task['title']}")
