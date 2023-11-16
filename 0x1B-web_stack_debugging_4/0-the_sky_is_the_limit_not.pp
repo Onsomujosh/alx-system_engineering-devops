8e4a2f83b2e5fccf3d37fe647c26e5d167fa9f0d
Onsomujosh <joshuaonsomu@gmail.com>
2023-11-16 15:32:40 -0800

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 # Fix the no of maximum open files per process
 
 exec { 'fix--for-nginx':
-  command => "/bin/sed -i /etc/default/nginx -e 's/15/3000/'"
-  onlyif  => 'test -e /etc/default/nginx',
+  command   => "/bin/sed -i /etc/default/nginx -e 's/15/3000/'"
+  onlyif    => 'test -e /etc/default/nginx',
   logoutput => true,
 }
 
 exec { 'restart nginx':
-  command => '/usr/sbin/service nginx restart',
-  require => Exec['fix--for-nginx']
+  command   => '/usr/sbin/service nginx restart',
+  require   => Exec['fix--for-nginx']
   logoutput => true,
 }
